e9d8878a36ef6b8648107aca3ab408381ad11070
Josh Duff <me@JoshDuff.com>
2016-08-19 20:54:37 -0500

    Properly parsing child lines into an object

--- transform-timeline-to-json.js
+++ transform-timeline-to-json.js
@@ -1,15 +1,7 @@
 const fs = require('fs')
 const markdown = fs.readFileSync('./timeline.md', { encoding: 'utf8' })
 
-function getLines(str) {
-	var match
-	const propertiesRegex = /^\t-\s*(.+)$/gm
-	const output = []
-	while ((match = propertiesRegex.exec(str)) !== null) {
-		output.push(match[1])
-	}
-	return output
-}
+
 
 function matches(str) {
 	var match
@@ -19,11 +11,32 @@ function matches(str) {
 	while ((match = regex.exec(str)) !== null) {
 		output.push({
 			title: match[1],
-			properties: getLines(match[2])
+			properties: turnLinesToObject(parseChildLines(match[2]))
 		})
 	}
 
 	return output
+}
+
+function parseChildLines(str) {
+	var match
+	const propertiesRegex = /^\t-\s*(.+)$/gm
+	const output = []
+	while ((match = propertiesRegex.exec(str)) !== null) {
+		output.push(match[1])
+	}
+	return output
+}
+
+function turnLinesToObject(lines) {
+	const o = {}
+
+	lines.forEach(str => {
+		const parts = str.split(/:\s*/)
+		o[parts[0].toLowerCase()] = parts.slice(1).join('')
+	})
+
+	return o
 }
 
 matches(markdown).forEach(str => console.log(str))
