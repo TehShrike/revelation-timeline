3a9fcdb98cf31b4fbd3cdfe24fd45c10f1eaf967
Josh Duff <me@JoshDuff.com>
2016-09-07 19:14:45 -0500

    It's a list of things with times. In a line.

---
+++ readme.md
@@ -0,0 +1,5 @@
+A timeline of historical events as they align with the prophesies in the book of Revelation.
+
+Data by Phil Kayser, as a part of his [sermon series on Revelation](http://www.dominioncovenantchurch.com/sermons/?series=36).
+
+# [View online](https://tehshrike.github.io/revelation-timeline)
--- transform-timeline-to-json.js
+++ transform-timeline-to-json.js
@@ -1,7 +1,35 @@
 const fs = require('fs')
+
+const longPropertyNames = {
+	h: 'hebrew',
+	s: 'macedonian',
+	g: 'gregorian',
+	r: 'sermon'
+}
+
+const datePropertyNames = [ 'h', 'm', 'g', 'day', 'amd' ]
+
 const markdown = fs.readFileSync('./timeline.md', { encoding: 'utf8' })
 
+const structure = matches(markdown).map(({ title, properties }) => {
+	const newEvent = {
+		title
+	}
+	copyAndElongatePropertyNames(properties, newEvent)
 
+	// castToInt(newEvent, 'day')
+	// gotta ignore 'day' until we figure out ranges with negative days
+
+	castToInt(newEvent, 'amd')
+	return newEvent
+})
+
+fs.writeFileSync('./timeline-data.js', `window.timelineData = ${JSON.stringify(structure)}`)
+
+function parseRange(str) {
+	const match = /^(-?[^\-]+) *- *([^\-]+)/.exec(str)
+	return match ? { start: match[1], end: match[2] } : { start: str, end: str }
+}
 
 function matches(str) {
 	var match
@@ -28,15 +56,43 @@ function parseChildLines(str) {
 	return output
 }
 
+function isShortDateFieldName(field) {
+	return datePropertyNames.indexOf(field) >= 0
+}
+
 function turnLinesToObject(lines) {
 	const o = {}
 
 	lines.forEach(str => {
 		const parts = str.split(/:\s*/)
-		o[parts[0].toLowerCase()] = parts.slice(1).join('')
+		const secondPart = parts.slice(1).join(':').trim()
+
+		if (secondPart) {
+			const shortFieldName = parts[0].toLowerCase()
+			o[shortFieldName] = isShortDateFieldName(shortFieldName) ? parseRange(secondPart) : secondPart
+		}
 	})
 
 	return o
 }
 
-matches(markdown).forEach(str => console.log(str))
+function copyAndElongatePropertyNames(o, target) {
+	Object.keys(o).forEach(key => target[longPropertyNames[key] || key] = o[key])
+}
+
+function castToInt(o, property) {
+	if (!o[property]) {
+		return
+	}
+	const definitelyTruthyValue = o[property]
+
+	switch (typeof definitelyTruthyValue) {
+		case 'string':
+			o[property] = parseInt(definitelyTruthyValue, 10)
+			break
+		case 'object':
+			castToInt(definitelyTruthyValue, 'start')
+			castToInt(definitelyTruthyValue, 'end')
+			break
+	}
+}
