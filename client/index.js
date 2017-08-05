1aed5c8e2ef7e3c40a2ba1d0c139544dc4c428fc
Josh Duff <me@JoshDuff.com>
2017-08-05 15:29:39 -0500

    Fix the bad things

--- client/index.js
+++ client/index.js
@@ -6,7 +6,7 @@ import Main from './Main.html'
 import timelineData from './timeline-data.js'
 
 const component = new Main({
-	target: document.querySelector('#container'),
+	target: document.querySelector('#target'),
 	data: {
 		timelineData, // no need to re-sort, is pre-sorted by transform-timeline-to-json.js
 		querystringParameters: getCurrentParameters(),
